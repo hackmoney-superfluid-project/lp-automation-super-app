5cf47e5ffa98ed73cfbe89fa93b49f31ed465c05
joshkokatnur <jkokatnur@gmail.com>
2022-05-16 17:37:24 -0400

    Converted main contract to factory

--- contracts/SuperApp.sol
+++ contracts/SuperApp.sol
@@ -1,97 +1,122 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity >=0.8.0;
 
-import { ISuperfluid, ISuperToken, ISuperApp, ISuperAgreement, SuperAppDefinitions } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
-import { SuperAppBase } from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBase.sol";
-import { CFAv1Library } from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
-import { IConstantFlowAgreementV1 } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+import {ISuperfluid, ISuperToken, ISuperApp, ISuperAgreement, SuperAppDefinitions} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import {SuperAppBase} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBase.sol";
+import {CFAv1Library} from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
+import {IConstantFlowAgreementV1} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+
+// import sub contract for user positions
+import "./UserPosition.sol";
+
+/* Uniswap required contracts */
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+
+//import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
 
 interface IERC20 {
-    function totalSupply() external view returns (uint);
+    function totalSupply() external view returns (uint256);
 
-    function balanceOf(address account) external view returns (uint);
+    function balanceOf(address account) external view returns (uint256);
 
-    function transfer(address recipient, uint amount) external returns (bool);
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
 
-    function allowance(address owner, address spender) external view returns (uint);
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
 
     function approve(address spender, uint256 amount) external returns (bool);
 
     function transferFrom(
         address sender,
         address recipient,
-        uint amount
+        uint256 amount
     ) external returns (bool);
 
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 }
 
 interface KeeperCompatibleInterface {
-  /**
-   * @notice checks if the contract requires work to be done.
-   * @param checkData data passed to the contract when checking for Upkeep.
-   * @return upkeepNeeded boolean to indicate whether the keeper should call
-   * performUpkeep or not.
-   * @return performData bytes that the keeper should call performUpkeep with,
-   * if upkeep is needed.
-   */
-  function checkUpkeep(bytes calldata checkData) external returns (bool upkeepNeeded,bytes memory performData);
-  
-  /**
-   * @notice Performs work on the contract. Executed by the keepers, via the registry.
-   * @param performData is the data which was passed back from the checkData
-   * simulation.
-   */
-  function performUpkeep(bytes calldata performData) external;
-    
+    /**
+     * @notice checks if the contract requires work to be done.
+     * @param checkData data passed to the contract when checking for Upkeep.
+     * @return upkeepNeeded boolean to indicate whether the keeper should call
+     * performUpkeep or not.
+     * @return performData bytes that the keeper should call performUpkeep with,
+     * if upkeep is needed.
+     */
+    function checkUpkeep(bytes calldata checkData)
+        external
+        returns (bool upkeepNeeded, bytes memory performData);
+
+    /**
+     * @notice Performs work on the contract. Executed by the keepers, via the registry.
+     * @param performData is the data which was passed back from the checkData
+     * simulation.
+     */
+    function performUpkeep(bytes calldata performData) external;
 }
 
 contract SuperAppPOC is KeeperCompatibleInterface, SuperAppBase {
+    /* --- Chain link --- */
+    // Used to ensure that the upkeep is perfomed every __interval__ seconds
+    uint256 public immutable interval;
+    uint256 public lastTimeStamp;
 
-    /**
-    * For chain link: Use an interval in seconds and a timestamp to slow execution of Upkeep
-    */
-    uint public immutable interval;
-    uint public lastTimeStamp;
-
+    /* --- Superfluid --- */
     using CFAv1Library for CFAv1Library.InitData;
-    CFAv1Library.InitData public cfaV1;      
-    bytes32 constant public CFA_ID = keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1");
-
+    CFAv1Library.InitData public cfaV1;
+    bytes32 public constant CFA_ID =
+        keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1");
+    IConstantFlowAgreementV1 cfa;
     uint256 flowEndTime;
-
     ISuperToken private _acceptedToken;
-    address public _receiver;  
+    address public _receiver;
     address public daiAddress = 0x15F0Ca26781C3852f8166eD2ebce5D18265cceb7;
     address public daixAddress = 0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f;
-    mapping(address => mapping (address => uint256)) allowed;
+    mapping(address => mapping(address => uint256)) allowed;
+    ISuperfluid _host;
+
+    /* --- Uniswap --- */
+    //INonfungiblePositionManager public immutable _nonfungiblePositionManager;
+
+    /* --- Contract storage --- */
+    // Track UserPosition contracts (map user address to contract address)
+    mapping(address => UserPosition) public userPositions;
 
     constructor(
         ISuperfluid host,
         ISuperToken acceptedToken,
-        address receiver
+        address receiver //, //INonfungiblePositionManager nonfungiblePositionManager
     ) payable {
         assert(address(host) != address(0));
         assert(address(acceptedToken) != address(0));
         assert(address(receiver) != address(0));
 
+        // uniswap
+        //_nonfungiblePositionManager = nonfungiblePositionManager;
+
         // chainlink vars
         interval = 10;
         lastTimeStamp = block.timestamp;
 
+        _host = host;
         _acceptedToken = acceptedToken;
         _receiver = receiver;
 
-        cfaV1 = CFAv1Library.InitData(
-            host,
-            IConstantFlowAgreementV1(
-                address(host.getAgreementClass(CFA_ID))
-            )
-        );
+        cfa = IConstantFlowAgreementV1(address(host.getAgreementClass(CFA_ID)));
+        cfaV1 = CFAv1Library.InitData(host, cfa);
 
-        uint256 configWord =
-            SuperAppDefinitions.APP_LEVEL_FINAL |
+        uint256 configWord = SuperAppDefinitions.APP_LEVEL_FINAL |
             SuperAppDefinitions.BEFORE_AGREEMENT_CREATED_NOOP |
             SuperAppDefinitions.BEFORE_AGREEMENT_UPDATED_NOOP |
             SuperAppDefinitions.BEFORE_AGREEMENT_TERMINATED_NOOP |
@@ -102,31 +127,40 @@ contract SuperAppPOC is KeeperCompatibleInterface, SuperAppBase {
     }
 
     function unwrap(ISuperToken _superToken) public {
+        // TODO: move unwrapping to child contracts
         _superToken.downgrade(1);
     }
 
-
-    function allowance(address owner, address delegate) public view returns (uint) {
+    function allowance(address owner, address delegate)
+        public
+        view
+        returns (uint256)
+    {
         return allowed[owner][delegate];
     }
 
     // need to approve the spend of ERC20 token
     function transfer(uint256 _amount) public {
         require(_amount > 0, "You need to sell at least some tokens");
-        uint256 currentAllowance = IERC20(daiAddress).allowance(msg.sender, address(this));
+        uint256 currentAllowance = IERC20(daiAddress).allowance(
+            msg.sender,
+            address(this)
+        );
         require(currentAllowance >= _amount, "Check the token allowance");
 
         IERC20(daiAddress).transferFrom(address(this), _receiver, _amount);
     }
 
-    function approve(address delegate, uint256 numTokens) public returns (bool) {
+    function approve(address delegate, uint256 numTokens)
+        public
+        returns (bool)
+    {
         allowed[msg.sender][delegate] = numTokens;
         return true;
     }
 
-
-
-    event StreamInitiated(ISuperToken _superToken, string message);
+    /* --- Superfluid callbacks --- */
+    event StreamInitiated(ISuperToken _superToken, string message, address newAddress);
 
     function afterAgreementCreated(
         ISuperToken _superToken,
@@ -142,37 +176,55 @@ contract SuperAppPOC is KeeperCompatibleInterface, SuperAppBase {
         onlyHost
         returns (bytes memory newCtx)
     {
-        emit StreamInitiated(_superToken, 'Stream initiated successfully');
-        newCtx = _ctx;
+        // get address of wallet that initiated stream (msg.sender would just point to this contract)
+        ISuperfluid.Context memory decompiledContext = _host.decodeCtx(_ctx);
+        (, int96 flowRate, , ) = cfa.getFlow(
+            _superToken,
+            decompiledContext.msgSender,
+            address(this)
+        );
+
+        // create new UserPosition contract
+        userPositions[decompiledContext.msgSender] = new UserPosition(decompiledContext.msgSender, _host, _acceptedToken, _receiver);//, _nonfungiblePositionManager);
+        address newPosAddress = address(userPositions[decompiledContext.msgSender]);
+
+        // emit event
+        emit StreamInitiated(_superToken, "Stream initiated successfully", newPosAddress);
+
+        // redirect stream to that contract and return new context
+        // TODO: subtract fee from initial flow?
+        newCtx = cfaV1.createFlowWithCtx(_ctx, newPosAddress, _acceptedToken, flowRate);
     }
 
-    // function afterAgreementUpdated(
-    //     ISuperToken _superToken,
-    //     address _agreementClass,
-    //     bytes32, // _agreementId,
-    //     bytes calldata, // _agreementData,
-    //     bytes calldata, // _cbdata,
-    //     bytes calldata _ctx
-    // )
-    //     external
-    //     override
-    //     onlyExpected(_superToken, _agreementClass)
-    //     onlyHost
-    //     returns (bytes memory newCtx)
-    // {
-    // }
+    function afterAgreementUpdated(
+        ISuperToken _superToken,
+        address _agreementClass,
+        bytes32, // _agreementId,
+        bytes calldata, // _agreementData,
+        bytes calldata, // _cbdata,
+        bytes calldata _ctx
+    )
+        external
+        override
+        onlyExpected(_superToken, _agreementClass)
+        onlyHost
+        returns (bytes memory newCtx)
+    {}
 
-    // function afterAgreementTerminated(
-    //     ISuperToken _superToken,
-    //     address _agreementClass,
-    //     bytes32, // _agreementId,
-    //     bytes calldata, // _agreementData
-    //     bytes calldata, // _cbdata,
-    //     bytes calldata _ctx
-    // ) external override onlyHost returns (bytes memory newCtx) {
-    //     if (!_isSameToken(_superToken) || !_isCFAv1(_agreementClass))
-    //         return _ctx;
-    // }
+    function afterAgreementTerminated(
+        ISuperToken _superToken,
+        address _agreementClass,
+        bytes32, // _agreementId,
+        bytes calldata, // _agreementData
+        bytes calldata, // _cbdata,
+        bytes calldata _ctx
+    ) external override onlyHost returns (bytes memory newCtx) {
+        // TODO: remove UserPosition contract from map
+        // TODO: do we close the position when stream ends? or we could just transfer the uniswap erc721 token to the user?
+
+        if (!_isSameToken(_superToken) || !_isCFAv1(_agreementClass))
+            return _ctx;
+    }
 
     function _isSameToken(ISuperToken superToken) private view returns (bool) {
         return address(superToken) == address(_acceptedToken);
@@ -196,18 +248,34 @@ contract SuperAppPOC is KeeperCompatibleInterface, SuperAppBase {
         _;
     }
 
-    /** Chainlink keeper required functions (https://docs.chain.link/docs/chainlink-keepers/compatible-contracts/) **/
-
-    function checkUpkeep(bytes calldata /* checkData */) external view override returns (bool upkeepNeeded, bytes memory /* performData */) {
+    /* --- Chainlink keeper required functions (https://docs.chain.link/docs/chainlink-keepers/compatible-contracts/) --- */
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    )
+        external
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
         upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
     }
 
-    function performUpkeep(bytes calldata /* performData */) external override {
-
-        // Revalidate the check
-        if ((block.timestamp - lastTimeStamp) > interval ) {
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        // Revalidate the check (perform this function every __interval__ seconds)
+        if ((block.timestamp - lastTimeStamp) > interval) {
             lastTimeStamp = block.timestamp;
-            _acceptedToken.downgrade(_acceptedToken.balanceOf(address(this)));
+
+            // Downgrade all super tokens in the contract
+            //_acceptedToken.downgrade(_acceptedToken.balanceOf(address(this)));
+
+            // TODO: Swap to opposite in token pair (DAI->ETH in this case)
+
+            // TODO: Deposit into uniswap
         }
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,16 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.13",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.13"
+      },
+      {
+        version: "0.7.6"
+      }
+    ]
+  },
   networks: {
     mumbai: {
       url: process.env.ALCHEMY_KEY,
